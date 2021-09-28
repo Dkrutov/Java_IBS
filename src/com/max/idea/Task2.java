2197f08a4f2fbf5b47d273dabcc2a71e2055010c
Dmitriy <dkrutov@applana.ru>
2021-09-28 19:55:30 +0300

    Изменеие Main. Создание Task2,Task3

--- src/com/max/idea/Task2.java
+++ src/com/max/idea/Task2.java
@@ -1,4 +1,11 @@
 package com.max.idea;
 
 public class Task2 {
+    public static void main(String[] args) {
+        /* Задача №2 - Типы данных */
+        double a = 3.1416;
+        byte b = 127;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+    }
 }
--- src/com/max/idea/Task3.java
+++ src/com/max/idea/Task3.java
@@ -1,4 +1,17 @@
 package com.max.idea;
 
 public class Task3 {
+    public static void main(String[] args) {
+        /* Задача №3_ Массивы */
+        int[] array1 = new int[] {1,2,3,4,5};
+        System.out.println("Элемент №0: " + array1[0]);
+        System.out.println("Элемент №4: " + array1[array1.length-1]);
+        int c=array1[0];
+        array1[0]=array1[array1.length-1];
+        array1[array1.length-1]=c;
+        System.out.println("Элемент №0: " + array1[0]);
+        System.out.println("Элемент №4: " + array1[4]);
+        int sum = array1[0] + array1[(array1.length-1)/2];
+        System.out.println("Сумма последнего и среднего элемента: " + sum);
+    }
 }
