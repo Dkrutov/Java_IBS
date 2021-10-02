60576a23a91d8a930927b749521fcb6a7158f28e
Dmitriy <dkrutov@applana.ru>
2021-10-02 14:52:38 +0300

    Task4

--- src/com/max/idea/Task4.java
+++ src/com/max/idea/Task4.java
@@ -1,4 +1,20 @@
 package com.max.idea;
-
+import java.util.Scanner;
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число x: ");
+        double x = in.nextInt();
+        System.out.println("Введите число y: ");
+        double y = in.nextInt();
+        System.out.println("Введите число z: ");
+        double z = in.nextInt();
+        double avr=(x+y+z)/3;
+        System.out.printf("Ваши числа: x=%.2f, y=%.2f, z=%.2f \n", x,y,z);
+        System.out.printf("Среднее арифметическое=%.2f \n", avr);
+        int a = (int)Math.floor(avr/2);
+        System.out.println("Сравниваемое значение с числом 3: " + a);
+        if (a > 3)
+            System.out.println("Программа выполнена корректно");
+    }
 }
