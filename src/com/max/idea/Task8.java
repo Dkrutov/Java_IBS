aa1f3d15f3b900487c597848b384da7501b68168
Dmitriy <dkrutov@applana.ru>
2021-10-10 00:58:00 +0300

    Task8 создание

---
+++ src/com/max/idea/Task8.java
@@ -0,0 +1,25 @@
+package com.max.idea;
+import java.util.Scanner;
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое положительное число:");
+        int sum=0;
+        int lengthArray = in.nextInt();
+        int[] array1 = new int[lengthArray];
+        for (int i=0;i<lengthArray;i++) {
+            array1[i]=i+1;
+            if (array1[i] % 2 != 0) {
+                sum+= array1[i];
+            }
+        }
+        /*
+        System.out.println("Массив заполнен!");
+        for (int i=0;i<lengthArray;i++) {
+            System.out.println("" + i + " элемент= " + array1[i]);
+        }
+         */
+        System.out.println("Сумма всех нечетных чисел = " + sum);
+
+    }
+}
