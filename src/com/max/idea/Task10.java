e8159186ad8dac9e88b8f516b3435f9031d09ca2
Dmitriy <dkrutov@applana.ru>
2021-10-10 00:58:37 +0300

    Task10 создание

---
+++ src/com/max/idea/Task10.java
@@ -0,0 +1,34 @@
+package com.max.idea;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Task10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число строк массива:");
+        int lengthArrayX = in.nextInt();
+        System.out.println("Введите число столбцов массива:");
+        int lengthArrayY = in.nextInt();
+        double[][] array2 = new double[lengthArrayX][lengthArrayY];
+        for (int i=0;i<lengthArrayX;i++) {
+            for (int j=0;j<lengthArrayY;j++) {
+                System.out.println("Введите " + i +", "+ j +" элемент массива");
+                array2[i][j]=in.nextDouble();
+            }
+        }
+        /*
+        for (int i=0;i<lengthArrayX;i++) {
+            for (int j=0;j<lengthArrayY;j++) {
+                System.out.println( i +", "+ j +" элемент массива: " + array2[i][j]);
+            }
+            System.out.println(" ");
+        }
+        */
+
+        System.out.println(Arrays.deepToString(array2));
+            for (int j=0;j<lengthArrayY;j++) {
+                System.out.println( 0 +" "+ j +" элемент массива * 3 = " + array2[0][j]*3);
+            }
+
+    }
+}
+
